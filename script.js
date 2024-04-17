a867a9e7b421838823f8441069eb285386c07b9c
Manish Yadav <manish.yadav.elit@gmail.com>
2024-04-17 15:37:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function playPause() {
+    var audioTrack = document.getElementById('audioTrack');
+    var controlBtn = document.getElementById('play-pause-button');
+
+    if (audioTrack?.paused) {
+        audioTrack.play();
+        //controlBtn.textContent = "Pause";
+        controlBtn.className = "pause";
+    } else {
+        audioTrack.pause();
+        //controlBtn.textContent = "Play";
+        controlBtn.className = "play";
+    }
+}
+
+audioTrack.addEventListener("ended", function () {
+    controlBtn.className = "play";
+});
